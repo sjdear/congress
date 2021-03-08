828e4a4b3333f9b1391ed16695c4e27b0f6cb27d
sjdear <67171108+sjdear@users.noreply.github.com>
2021-03-07 16:12:21 -0800

    Delete yesNo.js

--- yesNo.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.innerHTML === "Hello") {
-      x.innerHTML = "Swapped text!";
-    } else {
-      x.innerHTML = "Hello";
-    }
-  }
